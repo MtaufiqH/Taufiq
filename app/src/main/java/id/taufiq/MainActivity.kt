59c985592eb3a8e99697b48f1adf6b19843ac9fe
mtaufiqh62@gmail.com <123darussalam>
2019-07-14 19:40:05 +0800

    Dice Roll

---
+++ app/src/main/java/id/taufiq/MainActivity.kt
@@ -0,0 +1,40 @@
+package id.taufiq
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btn_roll.setOnClickListener {
+            rollDice()
+        }
+
+
+    }
+
+    private fun rollDice() {
+
+        val generateRandomNumber = Random().nextInt(6) + 1
+
+        val myDice = when(generateRandomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        myDiceId.setImageResource(myDice)
+    }
+}
