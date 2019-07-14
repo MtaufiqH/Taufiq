6ea6cf298598d53865477eba0f80d007137d0bd1
mtaufiqh62@gmail.com <123darussalam>
2019-07-14 22:46:19 +0800

    add VectorDrawable Dependency

--- app/src/main/java/id/taufiq/MainActivity.kt
+++ app/src/main/java/id/taufiq/MainActivity.kt
@@ -1,9 +1,7 @@
 package id.taufiq
 
-import android.media.Image
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
