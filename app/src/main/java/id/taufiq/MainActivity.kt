8517715cef103ef1d46b4012708d7bab83965d2c
mtaufiqh62@gmail.com <123darussalam>
2019-07-14 23:23:36 +0800

    layouting

--- app/src/main/java/id/taufiq/MainActivity.kt
+++ app/src/main/java/id/taufiq/MainActivity.kt
@@ -16,28 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        btn_roll.setOnClickListener {
-            rollDice()
-        }
-
-
     }
 
-    private fun rollDice() {
 
-        val generateRandomNumber = Random().nextInt(6) + 1
-
-        val myDice = when(generateRandomNumber){
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
-        }
-
-        myDiceId.setImageResource(myDice)
-    }
 }
