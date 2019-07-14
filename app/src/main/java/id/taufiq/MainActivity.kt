f0baca9656ec131114101eb1291ac0c07eef32c1
mtaufiqh62@gmail.com <123darussalam>
2019-07-14 22:50:41 +0800

    Roll Dice

--- app/src/main/java/id/taufiq/MainActivity.kt
+++ app/src/main/java/id/taufiq/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
+
+/*Create by Taufiq
+* Roll Dice App
+* */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
