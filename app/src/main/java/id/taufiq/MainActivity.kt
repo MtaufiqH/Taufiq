910fa3bf2018062ab704cc42d550181755b52630
mtaufiqh62@gmail.com <123darussalam>
2019-07-20 16:48:44 +0800

    Input nickname function

--- app/src/main/java/id/taufiq/MainActivity.kt
+++ app/src/main/java/id/taufiq/MainActivity.kt
@@ -1,10 +1,11 @@
 package id.taufiq
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-
 
 
 /*Create by Taufiq
@@ -15,6 +16,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        done_button.setOnClickListener { addNickname(it) }
+
+
+    }
+
+    private fun addNickname(view: View){
+        nickname.text = nickname_edit.text
+
+        view.visibility = View.GONE
+        nickname_edit.visibility = View.GONE
+        nickname.visibility = View.VISIBLE
+
+
+        //hide keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
 
     }
 
